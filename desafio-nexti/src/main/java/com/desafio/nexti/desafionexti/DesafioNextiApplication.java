aec186ea1b23b0b8f65275674e543cdaacd73c78
KevinKons <kevin.kons@hotmail.com>
2020-06-23 14:37:24 -0300

    .

--- desafio-nexti/src/main/java/com/desafio/nexti/desafionexti/DesafioNextiApplication.java
+++ desafio-nexti/src/main/java/com/desafio/nexti/desafionexti/DesafioNextiApplication.java
@@ -2,8 +2,10 @@ package com.desafio.nexti.desafionexti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class DesafioNextiApplication {
 
     public static void main(String[] args) {
